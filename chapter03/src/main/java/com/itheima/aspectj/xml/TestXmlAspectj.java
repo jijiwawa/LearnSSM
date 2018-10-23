a7e13a08e2b565174d3d9533096d2952b6dda5d3
jijiwawa <418850299@qq.com>
2018-10-23 08:21:47 +0800

    10.23

--- chapter03/src/main/java/com/itheima/aspectj/xml/TestXmlAspectj.java
+++ chapter03/src/main/java/com/itheima/aspectj/xml/TestXmlAspectj.java
@@ -1,17 +1,16 @@
-package com.itheima.factorybean;
+package com.itheima.aspectj.xml;
 
 import com.itheima.jdk.UserDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class ProxyFactoryBeanTest {
+public class TestXmlAspectj {
     public static void main(String[] args) {
-        String xmlPath ="applicationContext.xml";
+        String xmlPath ="applicationContext1.xml";
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
-        //从spring容器获取内容
-        UserDao userDao = (UserDao) applicationContext.getBean("userDaoProxy");
-        //执行方法
+        // 1 从spring容器获取内容
+        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
+        // 2 执行方法
         userDao.addUser();
-        userDao.deleteUser();
     }
 }
--- chapter03/src/main/java/com/itheima/jdk/UserDaoImpl.java
+++ chapter03/src/main/java/com/itheima/jdk/UserDaoImpl.java
@@ -1,11 +1,14 @@
 package com.itheima.jdk;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("userDao")
 public class UserDaoImpl implements UserDao {
     @Override
     public void addUser() {
         System.out.println("添加用户");
+        //int i =10/0;
     }
-
     @Override
     public void deleteUser() {
         System.out.println("删除用户 ");
